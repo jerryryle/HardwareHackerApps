c029bb70a649457c5502e2accf6a5f79a73ea5d1
Jerry Ryle <jerryryle@gmail.com>
2017-07-08 22:30:10 -0700

    Added CaptainHook C# application

---
+++ Windows/C#/CaptainHook/AboutBox.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CaptainHook
+{
+    partial class AboutBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
+            this.ok_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(205, 192);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(129, 47);
+            this.ok_button.TabIndex = 0;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Captain Hook, Version 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Copyright (C) 2017 Jerry Ryle";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CaptainHook.Properties.Resources.CaptainHookPNG;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AboutBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 251);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ok_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBox";
+            this.ShowIcon = false;
+            this.Text = "About Captain Hook";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Windows/C#/README.md
@@ -0,0 +1,18 @@
+# HardwareHackerApps - C&#35;
+
+To build the applications, open the HardwareHackerAppsCSharp.sln file in [Visual Studio 2017 Community<sup>1</sup>](https://www.visualstudio.com/downloads/).
+
+To create your own application based upon one of these applications, you should:
+
+1. Create your own empty solution in Visual Studio
+2. Copy the desired application project folder into your solution folder and add it to your solution
+
+
+## Folder descriptions
+
+* CaptainHook - A windowless application that hides in the notification area as an icon and demonstrates how to use a low-level hook to trap keyboard input.
+
+
+## References
+
+1. https://www.visualstudio.com/downloads/
